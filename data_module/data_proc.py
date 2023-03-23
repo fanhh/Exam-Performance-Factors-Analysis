491d35bb9a04abf81fec71cf178820d1b405714a
Xiang Ding <dingxiang987@gmail.com>
2023-03-22 23:02:45 -0700

    Update data_proc.py

--- data_module/data_proc.py
+++ data_module/data_proc.py
@@ -3,18 +3,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def average_score(row):
-    """Get the overall average exam score"""
+    """
+    Get the overall average exam score.
+    ---
+    parameters:
+    row = a row of dataframe
+    ---
+    Return a number
+    """
     total = row['math score'] + row['reading score'] + row['writing score']
-    return total/300
+    return total/3
 
 def quantify_binary(row):
-    """Quantify the string data into numbers"""
+    """
+    Quantify the string data into numbers.
+    ---
+    parameter:
+    row = a string
+    ---
+    Return an int
+    """
     if row in ['free/reduced', 'completed']:
         return 1
     return 0
 
 def education_qunaity(degree):
-    """Quantify the string data into numbers"""
+    """
+    Quantify the string data into numbers.
+    ---
+    parameter:
+    degree = a string
+    ---
+    Return a float
+    """
     if degree == "master's degree":
         return 1
     elif degree == "bachelor's degree":
@@ -24,20 +45,41 @@ def education_qunaity(degree):
     return 0.4
 
 def quantify_df(db):
-    """Create new columns in the dataframe for the linear models"""
+    """
+    Create new columns in the dataframe for the linear models.
+    ---
+    parameter:
+    db = a dataframe
+    ---
+    Return a dataframe
+    """
+    # quanity the parental level of education, lunch, test preparation course columns
+    # Replace all the old columns with the quanify ones
     return db.assign(overall_score = db.apply(average_score, axis=1), 
                    parental_level_of_education = db.get('parental level of education').apply(education_qunaity),
                    school_lunch = db.get('lunch').apply(quantify_binary),
                    exam_prep = db.get('test preparation course').apply(quantify_binary)).drop(
                    columns=['parental level of education','lunch', 'test preparation course'])
 
-def su(any_numbers):
-    """Convert number into standard units"""
-    return (any_numbers - any_numbers.mean()) / np.std(any_numbers)
+def su(numbers):
+    """
+    Convert number into standard units.
+    ---
+    parameter:
+    numbers = a series of numbers
+    ---
+    Return a float
+    """
+    return (numbers - numbers.mean()) / np.std(numbers)
 
 def letter_grade(grade):
     """
     Convert the oveall exam score to grade.
+    ---
+    Parameter:
+    grade = an float
+    ---
+    Return a string
     """
     if 90 <= grade <= 100:
         return 'A'
@@ -54,11 +96,17 @@ def grades_count(db, factor):
     Draw plot for the grades that's \
     lower than b distrubiton for each \
     factor.
+    ---
+    Parameters:
+    db = a dataframe
+    factor = a string
     """
+    # Assign a new column called score by apply function to the row
     grade_db = db.assign(score= db.apply(average_score, axis=1))
+    # Assign a new column called grade by apply function to the score column
     grade_db = grade_db.assign(grades= grade_db.get('score').apply(letter_grade))
+    # Filter out all the grades that's above a C
     grade_db = grade_db[(grade_db.get('grades') == 'F') | (grade_db.get('grades') == 'D')
                        |(grade_db.get('grades') == 'C')].groupby(factor).count().reset_index()
+    # Plot the bar chart
     grade_db.plot(kind='bar', x=factor, y='grades', title='Overall grades lower than B', figsize=(5,5))
-
-    
