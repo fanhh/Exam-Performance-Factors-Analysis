15eebb54d12d484161b5a5829615a2f84db197cb
Xiang Ding <dingxiang987@gmail.com>
2023-03-22 22:28:06 -0700

    Update data_proc.py

--- data_module/data_proc.py
+++ data_module/data_proc.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def average_score(row):
     """Get the overall average exam score"""
@@ -32,5 +34,31 @@ def quantify_df(db):
 def su(any_numbers):
     """Convert number into standard units"""
     return (any_numbers - any_numbers.mean()) / np.std(any_numbers)
+
+def letter_grade(grade):
+    """
+    Convert the oveall exam score to grade.
+    """
+    if 90 <= grade <= 100:
+        return 'A'
+    elif 80 <= grade < 90:
+        return 'B'
+    elif 70 <= grade < 80:
+        return 'C'
+    elif 60 <= grade < 70:
+        return 'D'
+    return 'F'
+
+def grades_count(db, factor):
+    """
+    Draw plot for the grades that's \
+    lower than b distrubiton for each \
+    factor.
+    """
+    grade_db = db.assign(score= db.apply(average_score, axis=1))
+    grade_db = grade_db.assign(grades= grade_db.get('score').apply(letter_grade))
+    grade_db = grade_db[(grade_db.get('grades') == 'F') | (grade_db.get('grades') == 'D')
+                       |(grade_db.get('grades') == 'C')].groupby(factor).count().reset_index()
+    grade_db.plot(kind='bar', x=factor, y='grades', title='Overall grades lower than B', figsize=(5,5))
 
     
